7f1641839d164d09e0cc57f7c49a3658e7b65b87
Omid Gholamalamdari <omid.alamdari@gmail.com>
2022-12-28 18:27:49 +0330

    rewrote code for tsv to xsl conversion

--- workflow/scripts/tsv_to_xlsx.py
+++ workflow/scripts/tsv_to_xlsx.py
@@ -4,4 +4,6 @@ import pandas as pd
 sys.stderr = open(snakemake.log[0], "w")
 
 data = pd.read_csv(snakemake.input.tsv, sep="\t")
-data.to_excel(snakemake.output.xlsx, index=False)
+xlWriter = pd.ExcelWriter(snakemake.output.xlsx, engine='xlsxwriter')
+data.to_excel(xlWriter, sheet_name='Sheet1')
+xlWriter.save()
